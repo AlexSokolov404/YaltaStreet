9390810fa91d064f9a5522e9e297e95ac1a756eb
AlexSokolov404 <sokolik.2000bk.ru@yandex.ru>
2025-03-30 00:08:03 +0300

    feat: add service worker

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,37 @@
+const CACHE_NAME = "yalta-streets-cache-v1";
+const urlsToCache = [
+  "/YaltaStreet",
+  "/index.html",
+  "/styles.css",
+  "/app.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
 self.addEventListener("install", (event) => {
-    console.log("Service Worker установлен");
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    console.log("Intercepting fetch request for:", event.request.url);
-  });
-  
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cache) => cache !== CACHE_NAME)
+          .map((cache) => caches.delete(cache))
+      );
+    })
+  );
+});
